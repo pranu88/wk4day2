ab07ede8dc44b44e426f1588e1b7929eba7c8ce0
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-24 10:48:27 -0400

    Q&Acompletes

---
+++ javascriptpractice.js
@@ -0,0 +1,29 @@
+// =========================
+//  VARIABLES AND DATATYPES
+// =========================
+// 1.How do we assign a value to a variable?
+// let color = 'red'
+
+// 2.How do we change the value of a variable?
+// color = 'blue'
+
+// 3.How do we assign an existing variable to a new variable?
+// let newColor = color
+
+// 4.Remind me, what are declare, assign, and define?
+// Declare: We create a variable by using keyword "let" or "const"
+// Assign: giving a value after declaring a variable
+// Define: giving a variable its initial datatype
+
+// 5.What is pseudocoding and why should you do it?
+// What is: pseudocoding is a way to write a description of a computer program or algorithm in plain english.
+// Why is: We should use to simplify and planning the program in small logical steps.
+
+// 6.What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// A reasonable amount spent in planning in solving the problem is between 50% to 70% and for coding it takes 50% to 30%.
+
+
+//======================= 
+//        STRINGS
+//=======================
+
